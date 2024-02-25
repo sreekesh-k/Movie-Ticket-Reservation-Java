f8d42c0bb810448001216a5c2e42a8ece2f48e05
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-02-25 20:53:52 +0530

    confirm

--- Confirmation.java
+++ Confirmation.java
@@ -16,6 +16,8 @@ public class Confirmation {
     }
 
     public void confirmBooking() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         List<Integer> selectedSeatIds = Session.getSelectedIds();
         double totalPrice = 0.0;
 
